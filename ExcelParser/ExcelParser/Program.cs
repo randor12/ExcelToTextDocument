9b46689b725b1bb98ae75f6393273cf5be74f212
randor12 <rynicholas@vt.edu>
2019-06-27 14:25:28 -0400

    Started program

---
+++ ExcelParser/ExcelParser/Program.cs
@@ -0,0 +1,26 @@
+/*******************************************
+ * The purpose of this program is to parse through text in an excel 
+ * sheet
+ * Ryan Nicholas
+ * Imperium 
+ * June 27, 2019
+ * 
+ *******************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExcelDataReader.Core;
+
+namespace ExcelParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
