1a23f3c6552aba6339c9ea81e20fd13cac10bff5
randor12 <rynicholas@vt.edu>
2019-06-27 16:13:21 -0400

    Version 1.0

--- ExcelParser/ExcelParser/Program.cs
+++ ExcelParser/ExcelParser/Program.cs
@@ -1,6 +1,7 @@
-/*******************************************
+﻿/*******************************************
  * The purpose of this program is to parse through text in an excel 
  * sheet
+ * 
  * Ryan Nicholas
  * Imperium 
  * June 27, 2019
@@ -12,15 +13,232 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ExcelDataReader.Core;
+using System.IO;
+using System.Text.RegularExpressions;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace ExcelParser
 {
     class Program
     {
+        private static int numFiles;
+        private static int passes;
+
+        /*
+         * Check for numbers
+         */
+        public static bool CheckNumbers(string word)
+        {
+            bool hasNumbers = false;
+
+            foreach (var c in word)
+            {
+                string input = c.ToString();
+                bool isNumber = Regex.IsMatch(input, "[0-9]", RegexOptions.Compiled);
+
+                if (isNumber)
+                {
+                    hasNumbers = true;
+                }
+            }
+
+            return hasNumbers;
+        }
+
+        /**
+         * Determines if a word is arabic
+         * @param text          Text
+         * @return isArabic         Returns true if the whole word is arabic
+         */
+        public static bool HasArabicGlyphs(string text)
+        {
+            char[] glyphs = text.ToCharArray();
+
+            foreach (char glyph in glyphs)
+            {
+                bool isArabic = false;
+
+                if (glyph >= 0x600 && glyph <= 0x6ff)
+                {
+                    isArabic = true;
+                }
+                if (glyph >= 0x750 && glyph <= 0x77f)
+                {
+                    isArabic = true;
+                }
+                if (glyph >= 0xfb50 && glyph <= 0xfc3f)
+                {
+                    isArabic = true;
+                }
+                if (glyph >= 0xfe70 && glyph <= 0xfefc)
+                {
+                    isArabic = true;
+                }
+
+                if (!isArabic)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /**
+         * The purpose of this function is to extract only the words that are arabic
+         */
+        public static List<string> GetArabicWords(string input)
+        {
+            string[] delimiters = new string[] { " ", "_", ".", "!", "\"", ":", ";", "#", "(", ")", ",", "'", "{", "}", "-", "%",
+            "؟", "،", "”", "“", "‘", "؛", "`", "?", ",", "+", "=", "*", "\\", "\n", "\t"};
+
+            string[] words = input.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            List<string> ArabicWords = new List<string>();
+
+            foreach (var word in words)
+            {
+                if (HasArabicGlyphs(word))
+                {
+                    if (!CheckNumbers(word))
+                    {
+                        if (word.Length <= 4000)
+                        {
+                            ArabicWords.Add(word);
+                        }
+                    }
+                    
+                }
+            }
+
+            return ArabicWords;
+        }
+
+        /*
+         * Reverse text
+         */
+        public static string Reverse(string text)
+        {
+            if (text == null) return null;
+            char[] array = text.ToCharArray();
+            Array.Reverse(array);
+            return new String(array);
+        }
+
+        /*
+         * Process files
+         * @param sDir          directory
+         */
+        public static void ProcessInputFiles(string sDir)
+        {
+            try
+            {
+                passes = 0;
+                numFiles = Directory.GetFiles(sDir, "*.csv", SearchOption.AllDirectories).Length;
+
+                /***************************************************************************************************
+                 * SELECT BELOW FOR ALL FILES WITHIN THE DIRECTORY (INCLUDING SUB FILES)
+                 * 
+                 * WILL NEED TO ADD "SearchOption.AllDirectories" below in for loop
+                 * 
+                 *numFiles = Directory.GetFiles(sDir, "*", SearchOption.AllDirectories).Length;
+                 ****************************************************************************************************/
+
+                foreach (string f in Directory.GetFiles(sDir, "*.csv", SearchOption.AllDirectories))
+                {
+                    passes++;
+                    Console.WriteLine("File: " + passes + " of " + numFiles);
+                    Console.WriteLine("File Name: " + f);
+                    ParseFile(f);
+                }
+
+
+            }
+            catch (System.Exception excpt)
+            {
+                Console.WriteLine(excpt);
+            }
+        }
+        
+        /*
+         * Process the data in the file
+         */
+        public static void ParseFile(string f)
+        {
+            string text = "";
+
+            Excel.Application excelApp = new Excel.Application
+            {
+                Visible = true
+            };
+
+            Excel.Workbook workbook = excelApp.Workbooks.Open(f, 0, false, 5, "", "", false, Excel.XlPlatform.xlWindows, 
+                "", true, false, 0, true, false, false);
+
+            Excel.Sheets excelSheet = workbook.Worksheets;
+            string currentSheet = Path.GetFileName(f);
+            int getEnd = currentSheet.IndexOf(".csv");
+            
+            if (getEnd > 0)
+            {
+                currentSheet = currentSheet.Substring(0, getEnd);
+            }
+
+            Excel.Worksheet excelWorksheet = (Excel.Worksheet)excelSheet.Item[currentSheet];
+
+            int totalRows = excelWorksheet.Rows.Count;
+
+            int column = 65;
+
+            for (int i = 1; i <= totalRows; i++)
+            {
+                var cell = (Excel.Range)excelWorksheet.Cells[i, column];
+                var GetString = (string)cell.Value;
+
+                text += GetString + "\n";
+            }
+
+            text = Regex.Replace(text, "<[^>]*>", string.Empty);
+
+            text = Regex.Replace(text, @"^\s*$\n", " ", RegexOptions.Multiline);
+
+            ProcessString(text);
+        }
+
+        /**
+         * Process the data retrieved from the excel sheet
+         */
+        public static void ProcessString(string s)
+        {
+            FileStream fileStream = new FileStream("Verbatim.txt", FileMode.Append);
+            
+            using (StreamWriter writer = new StreamWriter(fileStream))
+            {
+
+                List<string> AllArabicWords = GetArabicWords(s);
+
+                int count = 0;
+
+                foreach (string word in AllArabicWords)
+                {
+                    writer.WriteLine(word);
+                    count++;
+                }
+
+                Console.WriteLine("Number of words: " + count);
+                writer.Close();
+            }
+        }
+
         static void Main(string[] args)
         {
-            
+            string dir = "C:\\Users\\rnicholas\\Documents\\ArabicVerbatims";
+
+            Console.OutputEncoding = Encoding.Unicode;
+
+            ProcessInputFiles(dir);
+
+            Console.WriteLine("Completed");
         }
     }
 }
